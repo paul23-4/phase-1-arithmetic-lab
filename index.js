ac9570d43504fedbf23dcca342ceffc3aba403dd
paul <paul.thiongo@student.moringaschool.com>
2023-12-21 15:35:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 20, 15);
